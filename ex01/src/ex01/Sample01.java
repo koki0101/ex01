16dc38879670b25081deb4bfa0c13220c1a183fe
Koki Ono <aw.sao.bro@gmail.com>
2022-06-23 18:51:37 +0900

    コミット練習

---
+++ ex01/src/ex01/Sample01.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ex01;
+
+/**
+ * @author koki ono
+ *
+ */
+public class Sample01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world");
+
+	}
+
+}
