6af5aa3168c11feae3dbccc6049e0b4b9b81e6d8
Koki Ono <aw.sao.bro@gmail.com>
2022-07-26 19:10:14 +0900

    内容の変更

--- ex01/src/ex01/Sample01.java
+++ ex01/src/ex01/Sample01.java
@@ -13,10 +13,9 @@ public class Sample01 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		System.exit(0);
-		System.out.println("Hello world");
-		
+		if("apple" == "apple") {
+			System.out.println("Hello world");
+		}
 	}
 
 }
