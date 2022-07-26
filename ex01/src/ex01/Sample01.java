cb914faf2c88966c14b19c7cef8d2d93f5068cc8
Koki Ono <aw.sao.bro@gmail.com>
2022-07-26 19:21:47 +0900

    ソースコードの変更

--- ex01/src/ex01/Sample01.java
+++ ex01/src/ex01/Sample01.java
@@ -13,7 +13,17 @@ public class Sample01 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-			System.out.println("Hello world");
+		System.out.println("Hello world");
+		
+		String a = "apple";
+		String b = "apple";
+		
+		if(a == b) {
+			System.out.println("aとbは一緒です。");
+		}else {
+			System.out.println("aとbは違います。");
+		}
+			
 	}
 
 }
